d6e56da5e942c53e252bef4009f616c966bc810c
SavvasTheodorou <theodorou.savvas@gmail.com>
2018-04-06 16:20:17 +0000

    Finished hangman game

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,66 @@
 from .exceptions import *
+from random import choice
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ['blakclist', 'happy', 'blindspot']
 
 
 def _get_random_word(list_of_words):
-    pass
+    try:
+        random_word = choice(list_of_words)
+        return random_word
+    except:
+        raise InvalidListOfWordsException()
 
 
 def _mask_word(word):
-    pass
+    if not word:
+        raise InvalidWordException
+    my_masked_word = len(word) * '*'
+    return my_masked_word
+    
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    
+    masked_word_list = list(masked_word)
+    new_char = character.lower()
 
+    if not masked_word or len(answer_word) != len(masked_word):
+        raise InvalidWordException()
+        
+    if len(new_char) != 1:
+        raise InvalidGuessedLetterException()
+    
+    for idx, char in enumerate(answer_word):
+        if new_char == char.lower():
+            masked_word_list[idx] = new_char
+            masked_word = ''.join(masked_word_list)
+            
+    return masked_word
+    
+    
 
 def guess_letter(game, letter):
-    pass
+    
+    low_case_letter = letter.lower()
+    masked_word = _uncover_word(game['answer_word'], game['masked_word'], letter)
+    
+    if game['remaining_misses'] == 0 or game['masked_word'] == game['answer_word']:
+        raise GameFinishedException()
+        
+    game['masked_word'] = masked_word
+    game['previous_guesses'].append(low_case_letter)
+    
+    if low_case_letter not in game['answer_word'].lower():
+        game['remaining_misses'] -= 1
+    
+    if game['remaining_misses'] == 0:
+        raise GameLostException()
+    
+    if game['masked_word'] == game['answer_word']:
+        raise GameWonException()
+
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
